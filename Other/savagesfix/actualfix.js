4088c9020abde625f5b7f09bb1d03d060a9655e1
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-18 10:37:15 -0400

    June 18, 2022 10:37 AM

---
+++ Other/savagesfix/actualfix.js
@@ -0,0 +1,105 @@
+async function fetchMarketItems() {
+    const provider = new ethers.providers.JsonRpcProvider(providerAddress)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    // const data = await marketContract.fetchMarketItems()
+    let totalSupply = await marketContract.totalSupply()
+    totalSupply = totalSupply.toString()
+    var traded = 0
+    var numItems = 0
+    var prices = []
+    let found = false
+    let classicArtItems = []
+    let marketItems = []
+    for (var j=0; j < totalSupply; j++) {
+      let marketItem = await marketContract.getMarketItem(j+1)
+      marketItems.push(marketItem)
+    }
+    for (var j=0; j < marketItems.length; j++) {
+      if (marketItems[j].tokenId.toNumber() > 0 && marketItems[j].listed == true && marketItems[j].nftContract == nftmarketaddress) {
+        found = true
+        numItems++
+        const tokenUri = await marketContract.tokenURI(marketItems[j].tokenId)
+        const meta = await axios.get(tokenUri)
+        let price = ethers.utils.formatUnits(marketItems[j].price.toString(), 'ether')
+
+        prices.push(price)
+
+        let unix_timestamp = marketItems[j].timeListed.toString()
+        //console.log(nftVolume)
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000).toLocaleDateString('en-US')
+        let item = {
+          time: unix_timestamp,
+          date: date,
+          itemId: marketItems[j].itemId,
+          name: meta.data.name,
+          price,
+          listed: marketItems[j].listed,
+          tokenId: marketItems[j].tokenId.toNumber(),
+          seller: marketItems[j].seller,
+          owner: marketItems[j].owner,
+          image: meta.data.image,
+          discription: meta.data.description,
+        }
+
+        classicArtItems.push(item)
+      }
+      if (marketItems[j].nftContract == nftmarketaddress && marketItems[j].timeSold.toString() != 0) {
+        let unix_timestamp = marketItems[j].timeSold.toString()
+        traded += Number(marketItems[j].sellingPriceOne)
+        traded += Number(marketItems[j].sellingPriceTwo)
+        traded += Number(marketItems[j].sellingPriceThree)
+        traded += Number(marketItems[j].sellingPriceFour)
+      }
+    }
+    // const items = await Promise.all(
+    //   data.map(async (i) => {
+        // if (i.tokenId.toNumber() > 0 && i.listed == true && i.nftContract == nftmarketaddress) {
+        //   found = true
+        //   numItems++
+        //   const tokenUri = await marketContract.tokenURI(i.tokenId)
+        //   const meta = await axios.get(tokenUri)
+        //   let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+        //
+        //   prices.push(price)
+        //
+        //   let unix_timestamp = i.timeListed.toString()
+        //   nftVolume = i.sellingPriceOne + i.sellingPriceTwo + i.sellingPriceThree + i.sellingPriceFour
+        //   //console.log(nftVolume)
+        //   // Create a new JavaScript Date object based on the timestamp
+        //   // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        //   var date = new Date(unix_timestamp * 1000).toLocaleDateString('en-US')
+        //   let item = {
+        //     time: unix_timestamp,
+        //     date: date,
+        //     itemId: i.itemId,
+        //     name: meta.data.name,
+        //     price,
+        //     listed: i.listed,
+        //     tokenId: i.tokenId.toNumber(),
+        //     seller: i.seller,
+        //     owner: i.owner,
+        //     image: meta.data.image,
+        //     discription: meta.data.description,
+        //   }
+        //   return item
+        // }
+        // if (i.nftContract == nftmarketaddress && i.timeSold.toString() != 0) {
+        //   let unix_timestamp = i.timeSold.toString()
+        //   traded += Number(i.sellingPriceOne)
+        //   traded += Number(i.sellingPriceTwo)
+        //   traded += Number(i.sellingPriceThree)
+        //   traded += Number(i.sellingPriceFour)
+        // }
+    //   })
+    // )
+    setTotalItems(totalSupply)
+    if (found) {
+      prices.sort((a, b) => a - b)
+      setFloorPrice(prices[0])
+    }
+    setVolumeTraded(ethers.utils.formatUnits(traded.toString(), 'ether'))
+    setNumListedItems(numItems)
+    setNfts(classicArtItems)
+  }
