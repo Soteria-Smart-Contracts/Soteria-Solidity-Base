f89faf9d6a2f0b44b5f72c10f1e6dacd3ee4d5f7
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-17 21:28:50 -0400

    June 17, 2022 9:28 PM

--- Other/savagesfix/fix.js
+++ Other/savagesfix/fix.js
@@ -4,18 +4,9 @@ async function loginWithEth(){
     if(window.ethereum){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
-        await getID();
-        if (netID != 3){
-            console.log("The current Metamask/Web3 network is not Ropsten, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
-            alert("The current Metamask/Web3 network is not Ropsten, please connect to the Ropsten test network.");
-            showOverlay();
-            return("Failed to connect")
-        }
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
-        removeOverlay();
-        UpdateDetails();
         document.getElementById('WalletB').innerText = "Connected";
     } else {
         alert("No ETHER Wallet available")
