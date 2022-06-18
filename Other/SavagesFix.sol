d7b5344e1f0a094539c69fac789d2ad478e108ef
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-17 20:53:43 -0400

    June 17, 2022 8:53 PM

--- Other/SavagesFix.sol
+++ Other/SavagesFix.sol
@@ -8,4 +8,18 @@ contract arrayloopinjs {
         uint256 information1;
         uint256 information2;
     }
+
+    uint8[] structids;
+
+    mapping(uint256 => teststruct) structmapping;
+
+    function createsomestructs(uint256 howmany) public {
+        for(uint8 i = 0; i < howmany; i++){
+            teststruct memory newstruct;
+            newstruct.information1 = i;
+            newstruct.information2 = i;
+            structmapping[i] = newstruct;
+            structids.push(i);
+        }
+    }
 }
