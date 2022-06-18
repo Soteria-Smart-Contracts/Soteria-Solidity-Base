7520cf4788b51a287158b69612def926958e7bd3
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-17 21:37:42 -0400

    June 17, 2022 9:37 PM

--- Other/savagesfix/fix.js
+++ Other/savagesfix/fix.js
@@ -17,7 +17,7 @@ async function loginWithEth(){
 async function checknum(){ 
     let num = await contract.methods.lastnum().call();
     console.log(num);
-    
+    document.getElementsByClassName("num")[0].innerHTML = num;
     return num;
 }
 
