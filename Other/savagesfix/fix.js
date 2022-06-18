a3fb45ddc849ec8392369cca1913d1c7a7e35b08
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-17 21:40:27 -0400

    June 17, 2022 9:40 PM

--- Other/savagesfix/fix.js
+++ Other/savagesfix/fix.js
@@ -9,7 +9,7 @@ async function loginWithEth(){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
         accountarray = await web3.eth.getAccounts();
-        contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
+        contract = window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
     } else {
         alert("No ETHER Wallet available")
