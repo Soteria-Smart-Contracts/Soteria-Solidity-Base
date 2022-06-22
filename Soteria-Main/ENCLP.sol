23c92813ae76e1b5ef94de7c7d30b3526e04bd75
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-22 12:11:50 -0400

    June 22, 2022 12:11 PM

--- Soteria-Main/ENCLP.sol
+++ Soteria-Main/ENCLP.sol
@@ -250,6 +250,7 @@ contract EVM_NFT_Collateralized_Lending_Protocol {
 
     function InitializeCounterOffer(uint256 LoanID, bool ChangeTerm, uint256 COterm, bool ChangeInterest, uint256 COir, bool ChangeETH, uint256 COeth) internal returns(uint256 COID){
         require(UIDmapping[LoanMapping[LoanID].NFT_UID].Active == true);
+        if()
         require(COir > LoanMapping[LoanID].InterestRate);
 
     }
