5fce6247f830bccf852b6fed66724a22efe0ca2c
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-24 16:28:43 +0000

    Aug 24, 2022, 12:28 PM

--- MultiSignature Contracts/Native/2 Address.sol
+++ MultiSignature Contracts/Native/2 Address.sol
@@ -6,7 +6,7 @@ contract Multi_Signature{
     address public SignerOne;
     address public SignerTwo;
 
-    event ProposalCreated(uint256 Amount, address payable To, s )
+    event ProposalCreated(uint256 Amount, address payable To, string Memo)
 
     constructor(address _1, address _2){
         SignerOne = _1;
@@ -26,7 +26,7 @@ contract Multi_Signature{
     }
 
     function CreateProposal(uint256 Amount, address payable to, string memory Memo) public returns(bool success){
-
+        require 
     }
     
 
