6a65e454611316bc798214ddd224b735c3b516cb
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-24 15:55:10 +0000

    Aug 24, 2022, 11:55 AM

---
+++ MultiSignature Contracts/Native/2 Address.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:UNLICENSE
+pragma solidity ^0.8.7;
+
+contract Multi_Signature{
+    //Addresses
+    address public SignerOne;
+    address public SignerTwo;
+
+    constructor(address _1, address _2){
+        SignerOne = _1;
+        SignerTwo = _2;
+    }
+
+}
