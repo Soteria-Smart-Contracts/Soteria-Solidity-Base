0015985cf5da7ea3a9aaa1cd0d4ee7ca56f8954a
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-24 16:44:42 +0000

    Aug 24, 2022, 12:44 PM

--- MultiSignature Contracts/Native/2 Address.sol
+++ MultiSignature Contracts/Native/2 Address.sol
@@ -40,7 +40,8 @@ contract Multi_Signature{
     
 
     function ExecuteProposal(uint16 ProposalID) internal{
-        Amount = Proposals[ProposalID]
+        uint256 Amount = Proposals[ProposalID].Amount;
+        
 
     }
 
