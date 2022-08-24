88d581e41ec27d2bb48385bfea88d7f8fb711a27
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-24 17:03:39 +0000

    Aug 24, 2022, 1:03 PM

--- MultiSignature Contracts/Native/2 Address.sol
+++ MultiSignature Contracts/Native/2 Address.sol
@@ -49,16 +49,16 @@ contract Multi_Signature{
         ProposalSigned[ID][msg.sender] = true;
         Proposals[ID].Votes++;
 
-
+        
     }
 
     //Proposal Internals and executing
 
 
     function ExecuteProposal(uint16 ProposalID) internal{
-        require();
         uint256 Amount = Proposals[ProposalID].Amount;
         address payable Reciever = Proposals[ProposalID].Reciever;
+        require(Amount <= );
 
         Reciever.transfer(Amount);
 
