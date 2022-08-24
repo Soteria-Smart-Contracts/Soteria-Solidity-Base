276a7984586fc0efd18a2e354266716c6b1bd750
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-24 16:31:32 +0000

    Aug 24, 2022, 12:31 PM

--- MultiSignature Contracts/Native/2 Address.sol
+++ MultiSignature Contracts/Native/2 Address.sol
@@ -30,7 +30,7 @@ contract Multi_Signature{
     function CreateProposal(uint256 Amount, address payable to, string memory Memo) public returns(bool success){
         require(Signer[msg.sender] == true, "Not Signer");
 
-        Proposal.push()
+        Proposal.push(Amount, to, Memo, 0)
         
     }
     
