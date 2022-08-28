750cd9fdc924d092eb87a493254e8d2a95b083ee
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-28 20:17:29 +0000

    Aug 28, 2022, 4:17 PM

--- Soteria-Main/erc721standard.sol
+++ Soteria-Main/erc721standard.sol
@@ -1,4 +1,3 @@
-//SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
 
@@ -6,20 +5,25 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TESTETHCOST is ERC721Enumerable, Ownable {
+contract ClassicRewards is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 1 ether;
-  uint256 public maxSupply = 200;
-  uint256 public maxMintAmount = 20;
+  uint256 public cost = 2500000000 gwei;
+  uint256 public maxSupply = 100; //Check with PM
+  uint256 public maxMintAmount = 25;
+  uint256 public walletMaximum = 100;
+  bool public SpecificMintOn = true;
   bool public paused = false;
   bool public revealed = true;
   string public notRevealedUri;
   uint256[] public rand;
-  
 
+  //Minting Protocol based on Fisher-Yates Shuffle using mapping instead of array
+  mapping(uint256 => uint256) public UnMinted;
+  uint256 public MaxUnMinted = 100; //If this we're an array, this would be the equivalent of UnMinted.length
+  
 
   constructor(
     string memory _name,
@@ -42,15 +46,14 @@ contract TESTETHCOST is ERC721Enumerable, Ownable {
     require(_mintQuantity > 0);
     require(_mintQuantity <= maxMintAmount);
     require(supply + _mintQuantity <= maxSupply);
+    require((balanceOf(msg.sender) + _mintQuantity) <= walletMaximum);
 
     if (msg.sender != owner()) {
       require(msg.value >= cost * _mintQuantity);
     }
 
     for (uint256 i = 1; i <= _mintQuantity; i++) {
-      uint256 randomNumber  = _generateRandom(supply + i);
-      rand.push(randomNumber);
-      _safeMint(msg.sender, randomNumber);
+      RandomMint(msg.sender);
     }
   }
 
@@ -128,16 +131,121 @@ contract TESTETHCOST is ERC721Enumerable, Ownable {
     transferOwnership(newOwner);
   }
 
-  function _generateRandom(uint256 id) private view returns (uint256)
+  function _generateRandom(uint256 num) public view returns (uint256)
   {
        uint256 random;
         
-      random = uint256(keccak256
-        (abi.encodePacked(id, block.timestamp, msg.sender))) 
-        % maxSupply;
+       random = uint256(keccak256
+        (abi.encodePacked(block.timestamp, msg.sender, num))) 
+        % MaxUnMinted;
 
         return random;
 
   }
+
+  function RandomMint(address to) internal returns(bool success){
+    uint256 RandomNumber = _generateRandom(MaxUnMinted);
+
+    if(RandomNumber == 0){
+      RandomNumber = 1;
+    }
+
+    if(UnMinted[RandomNumber] == 0 && RandomNumber == MaxUnMinted){
+      _safeMint(to, RandomNumber);
+      --MaxUnMinted;
+      return(success);
+    }
+
+    if(UnMinted[RandomNumber] == 0){
+      _safeMint(to, RandomNumber);
+      if(UnMinted[MaxUnMinted] == 0){
+      UnMinted[RandomNumber] = MaxUnMinted;
+      --MaxUnMinted;
+      }
+      else{
+        UnMinted[RandomNumber] = UnMinted[MaxUnMinted];
+        --MaxUnMinted;
+      }
+    }
+    else{
+      _safeMint(to, UnMinted[RandomNumber]);
+      if(UnMinted[MaxUnMinted] == 0){
+      UnMinted[RandomNumber] = MaxUnMinted;
+      --MaxUnMinted;
+      }
+      else{
+        UnMinted[RandomNumber] = UnMinted[MaxUnMinted];
+        --MaxUnMinted;
+      }
+    }
+  }
+
+//Special Request Functions
+
+
+//Airdrop function
+   function Airdrop(address Reciever) public returns(bool success){
+       require(msg.sender == owner()); 
+       uint256 supply = totalSupply();
+       require(supply + 1 <= maxSupply);
+
+       RandomMint(Reciever);
+
+       return(success);
+   }
+
+
+
+//NFT Migration
+  function SMdisable() public returns(bool success){
+      require(msg.sender == owner()); 
+      SpecificMintOn = false;
+
+      return(success);
+  }
+
+  function SpecificMint(address to, uint256 ID) internal returns(bool success){
+    require (SpecificMintOn == true);
+
+    if(UnMinted[ID] == 0 && ID == MaxUnMinted){
+      _safeMint(to, ID);
+      --MaxUnMinted;
+      return(success);
+    }
+
+    if(UnMinted[ID] == 0){
+      _safeMint(to, ID);
+      if(UnMinted[MaxUnMinted] == 0){
+      UnMinted[ID] = MaxUnMinted;
+      --MaxUnMinted;
+      }
+      else{
+        UnMinted[ID] = UnMinted[MaxUnMinted];
+        --MaxUnMinted;
+      }
+    }
+    else{
+      _safeMint(to, UnMinted[ID]);
+      if(UnMinted[MaxUnMinted] == 0){
+      UnMinted[ID] = MaxUnMinted;
+      --MaxUnMinted;
+      }
+      else{
+        UnMinted[ID] = UnMinted[MaxUnMinted];
+        --MaxUnMinted;
+      }
+    }
+  }
+
+
+   function ArraySM(uint256[] memory IDs, address[] memory Recievers) public{
+       require(msg.sender == owner());
+
+       for(uint256 index; index < Recievers.length; index++){
+            require(IDs[index] <= 10000);
+            SpecificMint(Recievers[index], IDs[index]);
+        }
+       
+    }
   
 }
