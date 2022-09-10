1276b99b0363b309e868d92e329882afd0d5babd
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-10 19:08:25 +0000

    Sep 10, 2022, 3:08 PM

--- NFTholderscan/program.js
+++ NFTholderscan/program.js
@@ -1,1 +1,29 @@
 
+let ABI = window.abi;
+
+
+async function loginWithEth(){
+    if(window.ethereum){
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        window.web3 = await new Web3(ethereum);
+        await getID();
+        if (netID != 61){
+            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network."); //CHANGE FOR REAL CROWDSALE TO ETC
+            alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network.");
+            showOverlay();
+            return("Failed to connect")
+        }
+        accountarray = await web3.eth.getAccounts();
+        contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
+        account = accountarray[0];
+        if(await contract.methods.Eligibility(account).call() == false){
+            alert("This address is not on the eligibility list for the ClassicDAO private sale. If you signed up for this sale but see this message, make sure you are using the correct wallet. If issues persist, please contact us on discord, twitter or telegram.")
+            loginWithEth();
+        }
+        removeOverlay();
+        UpdateDetails();
+        document.getElementById('WalletB').innerText = "Connected";
+    } else {
+        alert("No ETHER Wallet available")
+    }
+}
