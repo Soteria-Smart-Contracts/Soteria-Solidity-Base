9d2c5279d3dad5201d795e52220ae23a1971ae05
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-10 19:08:46 +0000

    Sep 10, 2022, 3:08 PM

--- NFTholderscan/program.js
+++ NFTholderscan/program.js
@@ -9,13 +9,7 @@ async function loginWithEth(){
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
-        if(await contract.methods.Eligibility(account).call() == false){
-            alert("This address is not on the eligibility list for the ClassicDAO private sale. If you signed up for this sale but see this message, make sure you are using the correct wallet. If issues persist, please contact us on discord, twitter or telegram.")
-            loginWithEth();
-        }
-        removeOverlay();
-        UpdateDetails();
-        document.getElementById('WalletB').innerText = "Connected";
+
     } else {
         alert("No ETHER Wallet available")
     }
