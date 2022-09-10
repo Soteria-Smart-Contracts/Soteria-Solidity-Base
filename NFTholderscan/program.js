ca08358fad22507ef757957d3214791fb2dc7ca1
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-10 19:08:36 +0000

    Sep 10, 2022, 3:08 PM

--- NFTholderscan/program.js
+++ NFTholderscan/program.js
@@ -6,13 +6,6 @@ async function loginWithEth(){
     if(window.ethereum){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
-        await getID();
-        if (netID != 61){
-            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network."); //CHANGE FOR REAL CROWDSALE TO ETC
-            alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network.");
-            showOverlay();
-            return("Failed to connect")
-        }
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
