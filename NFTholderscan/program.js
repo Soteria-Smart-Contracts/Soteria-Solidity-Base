ba46a035049cd1f7b839c3f7d5287a2831724779
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-10 19:23:28 +0000

    Sep 10, 2022, 3:23 PM

--- NFTholderscan/program.js
+++ NFTholderscan/program.js
@@ -29,7 +29,7 @@ async function GetLists(HolderList){
         innerindex = 0;
         while(IDlist.length > innerindex){
             Addresses.push(HolderList[index]);
-            IDs.push(IDlist[innerindex])
+            IDs.push(IDlist[innerindex]);
             innerindex++;
         }
         index++;
