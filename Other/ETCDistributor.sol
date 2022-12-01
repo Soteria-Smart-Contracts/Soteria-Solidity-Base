5dc58b9374f8fb79c61cdf78b4cdb3f68019f349
Darius Tanz <scratchticketseb@gmail.com>
2022-12-01 13:25:03 +0000

    Dec 1, 2022, 8:25 AM

--- Other/ETCDistributor.sol
+++ Other/ETCDistributor.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSE
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NFTRewarddistributor{
+contract NFTRewardDistributor{
     //This contract uses ERC721 tokens to verify the ownership of
 }
