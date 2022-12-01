254971dcdbb15966f8a64bb71f5f833f74054752
Darius Tanz <scratchticketseb@gmail.com>
2022-12-01 13:52:19 +0000

    Dec 1, 2022, 8:52 AM

--- Other/NFTRewardDistributor.sol
+++ Other/NFTRewardDistributor.sol
@@ -28,6 +28,6 @@ contract NFTRewardDistributor{
 }
 
 interface ERC721{
-    function
+    function ownerOf(uint256 tokenId) external view returns (address owner);
     function walletOfOwner(address owner) external view returns(uint256[] memory IDs);
 }
