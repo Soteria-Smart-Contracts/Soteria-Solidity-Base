4b64c30ff359e74d1cb392d75850aa6517615b27
Darius Tanz <scratchticketseb@gmail.com>
2022-12-01 13:59:35 +0000

    Dec 1, 2022, 8:59 AM

--- Other/NFTRewardDistributor.sol
+++ Other/NFTRewardDistributor.sol
@@ -30,7 +30,7 @@ contract NFTRewardDistributor{
     //Internal functions
     function InitializeRewardInstance() internal{
         NewIdentifier = RewardInstances.length;
-        TotalEther
+        TotalEther = a
         RewardInstance NewInstance = RewardInstance(NewIdentifier, )
     }
 
