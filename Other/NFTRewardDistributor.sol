d1d4b28b6244f045fdef0c44152f9af568dc2c78
Darius Tanz <scratchticketseb@gmail.com>
2022-12-01 14:21:28 +0000

    Dec 1, 2022, 9:21 AM

--- Other/NFTRewardDistributor.sol
+++ Other/NFTRewardDistributor.sol
@@ -29,7 +29,7 @@ contract NFTRewardDistributor{
     //Public functions
     function GetTotalUnclaimed() public returns(uint256 TotalUnclaimed){
         uint256 TotalUnclaimed;
-        uint256[] Tokens = NFT
+        uint256[] Tokens = ERC721(NFT)
         for(uint256 index, index < ,index++)
     }
 
