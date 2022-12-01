5585cabd35ab910fec843185e0a0484f0490fef6
Darius Tanz <scratchticketseb@gmail.com>
2022-12-01 13:26:02 +0000

    Dec 1, 2022, 8:26 AM

--- Other/NFTRewardDistributor.sol
+++ Other/NFTRewardDistributor.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSE
 //This contract uses ERC721 tokens to verify the ownership of rewards before transfer
-//Created by SoteriaSC for 
+//Created by SoteriaSC for ShibaClassic/ClassicDawgs
 pragma solidity >=0.7.0 <0.9.0;
 
 contract NFTRewardDistributor{
