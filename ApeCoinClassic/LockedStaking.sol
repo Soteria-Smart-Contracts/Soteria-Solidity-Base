2642b9eb1521483e479fdf66469b26528f2dc815
Darius Tanz <scratchticketseb@gmail.com>
2023-01-09 19:49:09 +0000

    Jan 9, 2023, 2:49 PM

--- ApeCoinClassic/LockedStaking.sol
+++ ApeCoinClassic/LockedStaking.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 contract LockedStaking{
     //Variable Declarations
-    address CLD;
+    address ACE;
     address Operator;
     uint256 public TotalDeposits;
     bool PreSaleListCompleted = false;
@@ -15,8 +15,8 @@ contract LockedStaking{
     mapping(uint8 => uint256) LockTypeTime;
     mapping(uint8 => uint256) LockTypeMultiplier;
 
-    constructor(address _CLD){
-        CLD = _CLD; 
+    constructor(address _ACE){
+        ACE = _ACE; 
         Operator = msg.sender;
         LockTypeTime[1] = 2592000;//30 Days 2592000
         LockTypeTime[2] = 2592000;
@@ -53,9 +53,9 @@ contract LockedStaking{
 
     //Public Functions
     function CreateLock(uint8 Type, uint256 amount) public returns(bool success){
-        require(amount >= 1000000000000000000, "The minimum deposit for staking is 1 CLD");
-        require(ERC20(CLD).balanceOf(msg.sender) >= amount, "You do not have enough CLD to stake this amount");
-        require(ERC20(CLD).allowance(msg.sender, address(this)) >= amount, "You have not given the staking contract enough allowance");
+        require(amount >= 1000000000000000000, "The minimum deposit for staking is 1 ACE");
+        require(ERC20(ACE).balanceOf(msg.sender) >= amount, "You do not have enough ACE to stake this amount");
+        require(ERC20(ACE).allowance(msg.sender, address(this)) >= amount, "You have not given the staking contract enough allowance");
         require((ActiveLocks[msg.sender] + 1) <= 3);
 
         if(Type == 2 || Type == 4 || Type == 6 || Type == 8){
@@ -67,7 +67,7 @@ contract LockedStaking{
         uint256 Expiration = (block.timestamp + LockTypeTime[Type]);
         Lock memory NewLock = Lock(NewLockID, msg.sender, Type, amount, AmountOnWithdraw, Expiration);
 
-        ERC20(CLD).transferFrom(msg.sender, address(this), amount);
+        ERC20(ACE).transferFrom(msg.sender, address(this), amount);
 
         ActiveLocks[msg.sender] = ActiveLocks[msg.sender] + 1;
         UserLocks[msg.sender].push(NewLock);
@@ -82,7 +82,7 @@ contract LockedStaking{
             revert("This lock has already been claimed");
         }
         uint256 amount = UserLocks[msg.sender][ID].WithdrawAmount;
-        if(((ERC20(CLD).balanceOf(address(this)) - (UserLocks[msg.sender][ID].WithdrawAmount - UserLocks[msg.sender][ID].DepositAmount)) <= TotalDeposits)){ //This exists as protection in the case that the contract has not been refilled with CLD in time
+        if(((ERC20(ACE).balanceOf(address(this)) - (UserLocks[msg.sender][ID].WithdrawAmount - UserLocks[msg.sender][ID].DepositAmount)) <= TotalDeposits)){ //This exists as protection in the case that the contract has not been refilled with ACE in time
              amount = UserLocks[msg.sender][ID].DepositAmount; 
         }
 
@@ -95,7 +95,7 @@ contract LockedStaking{
 
         ActiveLocks[msg.sender] = ActiveLocks[msg.sender] - 1;
         
-        ERC20(CLD).transfer(msg.sender, amount);
+        ERC20(ACE).transfer(msg.sender, amount);
         return(success);
     }
 
