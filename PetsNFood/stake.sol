d070afeaa50aca1267c5f7eb4c1b4a38a6ad5fe3
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 19:01:36 +0000

    Jan 27, 2023, 2:01 PM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -78,7 +78,7 @@ contract LinuxPetStake{
     // }
 
     function ClaimRewards(uint256 PetID) public returns(bool success, uint256 Payout){
-        require(PetStakes[PetID].Staked == true && );
+        require(PetStakes[PetID].Staked == true && PetStakes[PetID].Staker == msg.sender);
 
     }
 
