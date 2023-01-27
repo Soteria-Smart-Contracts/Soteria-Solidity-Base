e65a14bae285071b2938caa911a580f10783e9d9
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 17:56:33 +0000

    Jan 27, 2023, 12:56 PM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -26,7 +26,7 @@ contract UnnamedStake{
             index++;
         }
 
-        FoodMultiplier = FoodIDs.length
+        FoodMultiplier = FoodIDs.length * 334;
         uint256 ROIPerSecond = BasePay / 31557600;
         PetStakes[PetID] = PetStake(PetID, FoodIDs.length, FoodIDs, 0);
         
