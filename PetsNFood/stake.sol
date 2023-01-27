932a707dbda6117df4affd746e49eac92f254dbe
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 18:48:44 +0000

    Jan 27, 2023, 1:48 PM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -71,7 +71,7 @@ contract UnnamedStake{
     }
 
     function StakeFood(uint256 PetID, uint256[] memory FoodIDs) public returns(bool success){
-        require(PetStakes);
+        require(PetStakes[PetID]);
     }
 
 
