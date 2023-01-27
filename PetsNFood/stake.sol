8046c3587453db5d7fee847a764142b43c0f0d37
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 16:45:42 +0000

    Jan 27, 2023, 11:45 AM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -8,7 +8,7 @@ contract UnnamedStake{
     uint256 public BasePay; //Yearly Base ROI in $TKN
     uint256 public FoodMultiplier;
 
-    mapping(address => )
+    mapping(address => mapping(uint))
 
     struct PetStake{
         uint256 PetID;
