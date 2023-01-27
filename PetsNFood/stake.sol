7dca3b3bdb7b8e0ad5e567e5efc3aab8a3cde234
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 16:30:38 +0000

    Jan 27, 2023, 11:30 AM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -32,7 +32,7 @@ contract UnnamedStake{
 }
 
 interface ERC721{
-     function balanceOf(address owner) external view returns (uint256 balance);
+    function balanceOf(address owner) external view returns (uint256 balance);
     function ownerOf(uint256 tokenId) external view returns (address owner);
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
