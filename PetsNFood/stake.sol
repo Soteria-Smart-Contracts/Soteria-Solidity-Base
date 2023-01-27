bd62ff52e238d7a486227847d61ac06c8a174de0
Darius Tanz <scratchticketseb@gmail.com>
2023-01-27 16:30:28 +0000

    Jan 27, 2023, 11:30 AM

--- PetsNFood/stake.sol
+++ PetsNFood/stake.sol
@@ -33,29 +33,7 @@ contract UnnamedStake{
 
 interface ERC721{
      function balanceOf(address owner) external view returns (uint256 balance);
-
-    /**
-     * @dev Returns the owner of the `tokenId` token.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must exist.
-     */
     function ownerOf(uint256 tokenId) external view returns (address owner);
-
-    /**
-     * @dev Safely transfers `tokenId` token from `from` to `to`.
-     *
-     * Requirements:
-     *
-     * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     * - `tokenId` token must exist and be owned by `from`.
-     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
-     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
-     *
-     * Emits a {Transfer} event.
-     */
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
     function transferFrom(address from, address to, uint256 tokenId) external;
