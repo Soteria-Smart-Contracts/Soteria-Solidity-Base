5a7451b9bb84f06c461bcce738402b1d6416a0fe
Darius Tanz <scratchticketseb@gmail.com>
2023-01-31 01:45:29 +0000

    Jan 30, 2023, 8:45 PM

--- PetsNFood/LinuxStake.sol
+++ PetsNFood/LinuxStake.sol
@@ -14,7 +14,7 @@ contract LinuxPetStake{
 
     //All stakes stored here
     mapping(uint256 => PetStake) public PetStakes; //TODO: TEST
-    mapping(address => uint256)
+    mapping(address => uint256) public StakedPets;
 
     struct PetStake{
         bool Staked;
