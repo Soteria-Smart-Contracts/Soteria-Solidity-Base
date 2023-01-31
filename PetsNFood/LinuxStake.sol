dbd1cedb5f3de0dfbe5b81568cfb9cdcbc7dff4e
Darius Tanz <scratchticketseb@gmail.com>
2023-01-31 19:22:52 +0000

    Jan 31, 2023, 2:22 PM

--- PetsNFood/LinuxStake.sol
+++ PetsNFood/LinuxStake.sol
@@ -244,7 +244,6 @@ contract LinuxPetStake{
     }
 
 
-
 }
 
 interface ERC721{
@@ -261,11 +260,11 @@ interface ERC721{
 }
 
 interface ERC20 {
-  function balanceOf(address owner) external view returns (uint256);
-  function allowance(address owner, address spender) external view returns (uint256);
-  function approve(address spender, uint value) external returns (bool);
-  function Mint(address _MintTo, uint256 _MintAmount) external;
-  function transfer(address to, uint value) external returns (bool);
-  function transferFrom(address from, address to, uint256 value) external returns (bool); 
-  function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint value) external returns (bool);
+    function Mint(address _MintTo, uint256 _MintAmount) external;
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool); 
+    function totalSupply() external view returns (uint);
 }  
