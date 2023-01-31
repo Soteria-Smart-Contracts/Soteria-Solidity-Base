46dadbc895e956b3edc036e7486a819e8f12a085
Darius Tanz <scratchticketseb@gmail.com>
2023-01-31 01:49:46 +0000

    Jan 30, 2023, 8:49 PM

--- PetsNFood/LinuxStake.sol
+++ PetsNFood/LinuxStake.sol
@@ -39,7 +39,8 @@ contract LinuxPetStake{
         uint256 ROIPerSecond = (BasePay / 600); //TODO: TEST
         PetStakes[PetID] = PetStake(true, msg.sender, 0, EmptyArray, ROIPerSecond, block.timestamp);
 
-        StakedPets[msg.sender].push
+        StakedPets[msg.sender].push(PetID);
+        PetIndex
 
         emit PetStaked(PetID, msg.sender);
         return(success);
