869de9c849e5fad5da21fc4c67fccfe0ec07cf76
Darius Tanz <scratchticketseb@gmail.com>
2023-01-31 19:17:58 +0000

    Jan 31, 2023, 2:17 PM

--- PetsNFood/LinuxStake.sol
+++ PetsNFood/LinuxStake.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.17;
 
 contract LinuxPetStake{
     //Core Variables
-    address public Pets = 0x000000000000000000000000000;
-    address public Food = 0xF98b0713375793184E49526a243D799a5179735a;
-    address public LinuxToken = 0x369Acc7aaE208F59f0a4043A534943cfd7C0a066;
+    address public Pets = 0x0000000000000000000000000000000000000000; //Replace 
+    address public Food = 0x0000000000000000000000000000000000000000;
+    address public LinuxToken = 0x0000000000000000000000000000000000000000;
     address public Owner;
     uint256 public BasePay = 100000000000000000000; //Yearly Base ROI in $LinuxToken
     uint256 public FoodBoost = 1000; // in 0.0001 of percentage
