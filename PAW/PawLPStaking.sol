6d3ffdad21990dc660c6afd4c1570ea468129017
Darius Tanz <scratchticketseb@gmail.com>
2023-02-01 19:23:37 +0000

    Feb 1, 2023, 2:23 PM

--- PAW/PawLPStaking.sol
+++ PAW/PawLPStaking.sol
@@ -1,127 +1,121 @@
-// SPDX-License-Identifier: UNLICENSE
-pragma solidity ^0.8.17;
+//SPDX-License-Identifier:UNLICENSE
+pragma solidity ^0.8.7;
 
-contract PAWPairStaking{
-    address Creator;
-    address PairContract;
-    address PAW;
-    string public PAWPair;
-    uint Multiplier;
-    uint OnOff;
-    
-    mapping(address => uint256) Staked;
-    mapping(address => uint256) ClaimablePAW;
-    mapping(address => uint256) BlockDeposit;
-    
-    constructor(address payable _PAW, address payable _PairContract, string memory _Pair){
-        PairContract = _PairContract;
+
+contract FlexibleStaking{
+    //Variable and other Declarations
+    address public PAW;
+    address public PairContract;
+    uint256 public TotalDeposits;
+    uint256 public RewardMultiplier = 7927448; //Default set at 1%
+    bool PreSaleListCompleted = false;
+    address public Operator;
+
+    //Add Total Staked (for projections)
+
+    mapping(address => uint256) public Deposits;
+    mapping(address => uint256) public LastUpdateUnix;
+
+    //Events
+    event Deposited(uint256 NewBalance, address user);
+    event Withdrawn(uint256 NewBalance, address user);
+    event Claimed(uint256 Amount, address user);
+    event ReInvested(uint256 NewBalance, address user);
+
+
+    constructor(address _PAW, address _Pair){
         PAW = _PAW;
-        PAWPair = _Pair;
-        Creator = msg.sender;
-    }
-    
-    event Deposit(address indexed sender, uint indexed amount);
-    event Withdraw(address indexed sender, uint256 indexed amount);
-    
-    function Stake(uint256 _amount) public payable returns(bool success){
-        require (ERC20(PairContract).balanceOf(msg.sender) >= _amount);
-        require (ERC20(PairContract).allowance(msg.sender,(address(this))) >= _amount);
-        require (OnOff == 1);
-        
-        ERC20(PairContract).transferFrom(msg.sender, (address(this)), _amount);
-        
-        if (Staked[msg.sender] > 0){
-            ClaimablePAW[msg.sender] = UnclaimedPAW(msg.sender);
-        }
-        Staked[msg.sender] = Staked[msg.sender]+(_amount);
-        BlockDeposit[msg.sender] = block.number;
-        
-        emit Deposit (msg.sender, _amount);
-        return success;
-        
-        
-    }
-    
-    
-    function ClaimPAW() public payable returns(bool success){
-        require (Staked[msg.sender] > 0);
-        require (OnOff == 1);
-        
-        ClaimablePAW[msg.sender] = UnclaimedPAW(msg.sender);
-        
-        ERC20(PAW).Mint(msg.sender, ClaimablePAW[msg.sender]);
-        
-        emit Withdraw(address(this),ClaimablePAW[msg.sender]);
-        
-        ClaimablePAW[msg.sender] = 0;
-        BlockDeposit[msg.sender] = block.number;
-        return success;
-    }
-    
-    
-    function Unstake(uint256 _amount) public payable returns(bool success){
-        require (Staked[msg.sender] > 0);
-        require (Staked[msg.sender] >= _amount);
-        
-        ClaimablePAW[msg.sender] = UnclaimedPAW(msg.sender);
-        
-        ERC20(PAW).Mint(msg.sender, ClaimablePAW[msg.sender]);
-        ERC20(PairContract).transfer(msg.sender, _amount);
-        
-        Staked[msg.sender] = Staked[msg.sender]-(_amount);
-        BlockDeposit[msg.sender] = block.number;
-        ClaimablePAW[msg.sender] = 0;
-        
-        return success;
-    }
-    
-    function StakedELP(address Staker) public view returns(uint256){
-        return Staked[Staker];
-    }
-    
-    function UnclaimedPAW(address Staker) public view returns(uint256){
-        return ClaimablePAW[Staker]+((((CalculatePAWequivalent(Staked[Staker])*(12594*(block.number-(BlockDeposit[Staker]))))/10000000000)/1000)*Multiplier);
-    }
-    
-    function TotalStaked()public view returns(uint256){
-        return ERC20(PairContract).balanceOf(address(this));
+        PairContract = _Pair;
+        Operator = msg.sender;
     }
 
-    function CalculatePAWequivalent(uint256 _amount) public view returns(uint256){
-        return (((ERC20(PAW).balanceOf(PairContract)*((((1000000000000000000 * _amount) / (Pair(PairContract).totalSupply())))) / 1000000000000000000))*2);
+
+    //Public Functions
+    function Deposit(uint256 amount) public returns(bool success){  
+        require(amount >= 100000000000, "The minimum deposit for staking is 0.0000001 LP");
+        require(ERC20(PairContract).balanceOf(msg.sender) >= amount, "You do not have enough LP to stake this amount");
+        require(ERC20(PairContract).allowance(msg.sender, address(this)) >= amount, "You have not given the staking contract enough allowance");
+
+        if(Deposits[msg.sender] > 0){
+            Claim();
+        }
+
+        Update(msg.sender);
+        ERC20(PairContract).transferFrom(msg.sender, address(this), amount);
+        TotalDeposits = TotalDeposits + amount;
+        Deposits[msg.sender] = (Deposits[msg.sender] + amount);
+
+        emit Deposited(Deposits[msg.sender], msg.sender);
+        return(success);
     }
-    
-    function CallPair() public view returns(uint256){
-        return Pair(PairContract).totalSupply();
+
+    function Withdraw(uint256 amount) public returns(bool success){
+        require(Deposits[msg.sender] >= amount);
         
+        if((ERC20(PAW).balanceOf(address(this)) >= (GetUnclaimed(msg.sender)))){
+            Claim();
+        }
+
+        Deposits[msg.sender] = Deposits[msg.sender] - amount;
+        TotalDeposits = TotalDeposits - amount;
+        ERC20(PairContract).transfer(msg.sender, amount);
+        
+        emit Withdrawn(Deposits[msg.sender], msg.sender);
+        return(success);
     }
-    
-    //Creator functions
-    
-    function Toggle(uint OneOnTwoClosed) public returns(bool success){
-        require (msg.sender == Creator);
-        if (OneOnTwoClosed == 1){
-            OnOff = 1;
-            } else if(OneOnTwoClosed == 2){
-                OnOff = 2;
-            } else {
-                OnOff = 2;
-            }
-            
-            return success;
+
+
+    function Claim() public returns(bool success){
+        uint256 Unclaimed = GetUnclaimed(msg.sender);
+        require(Unclaimed > 0);
+
+        Update(msg.sender);
+
+        ERC20(PAW).transfer(msg.sender, Unclaimed);
+        
+        emit Claimed(Unclaimed, msg.sender);
+        return(success);
     }
-    
+
+    //OwnerOnly Functions
+
     function ChangeMultiplier(uint256 NewMultiplier) public returns(bool success){
-        require (msg.sender == Creator);
-        require (NewMultiplier >= 100 && NewMultiplier <= 10000);
+        require(msg.sender == Operator);
+
+        RewardMultiplier = NewMultiplier;
+
+        return(success);
+    }
+
+    function ChangePairAddress(address NewPair) public returns(bool success){
+        require(msg.sender == Operator);
+
+        PairContract = NewPair;
+
+        return(success);
+    }
+
+    //Internal Functions
+    function Update(address user) internal{
+        LastUpdateUnix[user] = block.timestamp;
+    }
+
+    //Functional view functions
+
+    function GetUnclaimed(address user) public view returns(uint256){
+        uint256 Time = (block.timestamp - LastUpdateUnix[user]);
+        uint256 Unclaimed;
         
-        Multiplier = NewMultiplier;
-        return success;
+        Unclaimed = (((RewardMultiplier * Time) * CalculatePAWequivalent(Deposits[user])) / 1000000000000000); // 7927448 per %
+
+        return(Unclaimed);
+    }
+
+    function CalculatePAWequivalent(uint256 amount) public view returns(uint256){
+        return (((ERC20(PAW).balanceOf(PairContract)*((((1000000000000000000 * amount) / (ERC20(PairContract).totalSupply())))) / 1000000000000000000))*2);
     }
 
 }
-
-
 
 interface ERC20 {
   function balanceOf(address owner) external view returns (uint256);
@@ -132,7 +126,3 @@ interface ERC20 {
   function transferFrom(address from, address to, uint256 value) external returns (bool); 
   function totalSupply() external view returns (uint);
 }    
-
-interface Pair {
-    function totalSupply() external view returns (uint);
-}
