f3492a7b1f17eda1926d7b2b26a93b7393fbb153
Darius Tanz <scratchticketseb@gmail.com>
2023-02-01 18:44:23 +0000

    Feb 1, 2023, 1:44 PM

--- PAW/temp.sol
+++ PAW/temp.sol
@@ -126,7 +126,7 @@ contract FlexibleStaking{
     }
 
     function CalculatePAWequivalent(uint256 _amount) public view returns(uint256){
-        return (((ERC20(PAW).balanceOf(PairContract)*((((1000000000000000000 * _amount) / (Pair(PairContract).totalSupply())))) / 1000000000000000000))*2);
+        return (((ERC20(PAW).balanceOf(PairContract)*((((1000000000000000000 * _amount) / (ERC20(PairContract).totalSupply())))) / 1000000000000000000))*2);
     }
 
 }
