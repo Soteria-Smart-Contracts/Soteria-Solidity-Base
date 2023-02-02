067b38d61ab72070abb0ef3ab8a98bec90fe7208
Darius Tanz <scratchticketseb@gmail.com>
2023-02-02 13:21:54 +0000

    Feb 2, 2023, 8:21 AM

--- PAW/PawFlexStaking.sol
+++ PAW/PawFlexStaking.sol
@@ -50,7 +50,7 @@ contract FlexibleStaking{
         require(Deposits[msg.sender] >= amount);
         
         if((ERC20(PAW).balanceOf(address(this)) - (GetUnclaimed(msg.sender))) >= TotalDeposits){
-        Claim();
+            Claim();
         }
 
         Deposits[msg.sender] = Deposits[msg.sender] - amount;
