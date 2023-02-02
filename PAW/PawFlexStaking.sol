5ef104afbb94e1940cffe498bf596f111f9fbd0b
Darius Tanz <scratchticketseb@gmail.com>
2023-02-02 13:24:10 +0000

    Feb 2, 2023, 8:24 AM

--- PAW/PawFlexStaking.sol
+++ PAW/PawFlexStaking.sol
@@ -6,7 +6,7 @@ contract FlexibleStaking{
     //Variable and other Declarations
     address public PAW;
     uint256 public TotalDeposits;
-    uint256 public RewardMultiplier = 7927448; //Default set at 1%
+    uint256 public RewardMultiplier = 69444444480; //Default set at 1% 7927448
     address public Operator;
 
     //Add Total Staked (for projections)
