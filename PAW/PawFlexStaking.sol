01ffbf616679588d77e68814079f8eba37ec018c
Darius Tanz <scratchticketseb@gmail.com>
2023-02-06 13:15:38 +0000

    Feb 6, 2023, 8:15 AM

--- PAW/PawFlexStaking.sol
+++ PAW/PawFlexStaking.sol
@@ -24,10 +24,10 @@ contract FlexibleStaking{
     event ReInvested(uint256 NewBalance, address user);
 
 
-    constructor(address _PAW, address _wPAW, uint256 APY){ // APY is 0.1% to 1, so 1% is 10, 10% is 100, etc...
+    constructor(address _PAW, address _wPAW, uint256 _APY){ // APY is 0.1% to 1, so 1% is 10, 10% is 100, etc...
         PAW = _PAW;
         wPAW = _wPAW;
-        APY 
+        APY = APY;
         RewardMultiplier = APY * 792744;
         Operator = msg.sender;
     }
