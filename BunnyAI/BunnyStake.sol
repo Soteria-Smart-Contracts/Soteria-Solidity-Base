fe75d02638d9bae203a7c280cb9cd510fa727b82
Darius Tanz <scratchticketseb@gmail.com>
2023-02-26 17:18:27 +0000

    Feb 26, 2023, 12:18 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -29,12 +29,12 @@ contract BunnyDualStake{
     }
 
     //Add to NFT with existing BUNAI lock
-    function AddNFTtoLock(uint256 LockID, uint256[] calldata NFTs) public returns(bool success){
+    function AddNFTtoLock(uint256 UserLockID, uint256[] calldata NFTs) public returns(bool success){
 
     }
 
     //Claim BUNAILock
-    function ClaimLock(uint256 LockID)
+    function ClaimLock(uint256 UserLockID)
 
     //Owner Only Functions
 
