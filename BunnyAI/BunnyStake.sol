17003592433c6e6fb85b7fb0b8637601a9661758
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 14:01:32 +0000

    Mar 1, 2023, 9:01 AM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -35,7 +35,7 @@ contract BunnyDualStake{
     //Make events, constructor, etc...
     constructor(){
         LockLenghts[0] = 864000; //TODO: 10 days
-        LockLenghts[1] = 2630000; //TODO: 30 days
+        LockLenghts[1] = 2592000; //TODO: 30 days
         LockLenghts[2] = 7890000; //TODO: 90 days
         LockPayouts[0] = 5000;
         LockPayouts[1] = 10000;
