dbe4637c11aa6cf06d8f7750175b51bba2776465
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 01:37:35 +0000

    Feb 28, 2023, 8:37 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -8,7 +8,7 @@ contract BunnyDualStake{
     uint256 public BaseAPR; //The base APR yearly in %
     uint256 public SecondsAPR; //The base APR per second in BUNAI
     uint256 public NFTBoostMultiplier; //APR Booster in Basis Points
-    uint256 public MinimumStake;
+    uint256 public MinimumStake; //The minimum amount 
 
     struct Lock{
         uint256 LockStart; //Unix Time
