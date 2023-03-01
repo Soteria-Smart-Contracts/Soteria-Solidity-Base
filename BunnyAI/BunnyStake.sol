e9f1b3ce1b58c3497761bebd24b05d33ffe1f75e
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 13:54:31 +0000

    Mar 1, 2023, 8:54 AM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -47,6 +47,7 @@ contract BunnyDualStake{
     function LockBUNAI(uint256 BUNAI_Amount, LockOptions Type) public returns(bool success){
         require(BUNAI_Amount >= MinimumStake, 'You must stake atleast the minimum stake Amount');
         require(ERC20(BUNAI).transferFrom(msg.sender, address(this), BUNAI_Amount), 'Unable to transfer BUNAI to contract');
+        require(TransferInNFTs())
 
         uint256 EndTime = (block.timestamp + LockLenghts[Type]);
         uint256 Payout = ((amount * LockPayouts[Type]) / 10000) + amount;
