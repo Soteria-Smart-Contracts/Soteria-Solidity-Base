456248d25f304a44e882d642518d90708d47f468
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 14:59:33 +0000

    Mar 1, 2023, 9:59 AM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -82,7 +82,7 @@ contract BunnyDualStake{
 
         UpdateBoostList(UserLockID, NFTs);
         uint256 BoostedPayoutMultiplier =  UserLocks[msg.sender][UserLockID].TotalMultiplier + (NFTBoostMultiplier * NFTs.length);
-        uint256 NewPayout = UserLocks[msg.sender][UserLockID].BN
+        uint256 NewPayout = UserLocks[msg.sender][UserLockID].BUNAI_Locked
 
         return(success);
     }
