4ad1681a007dd851eabc8d3db4b6aeadb4d7c974
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 14:05:11 +0000

    Mar 1, 2023, 9:05 AM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -75,7 +75,7 @@ contract BunnyDualStake{
 
     //Add to NFT with existing BUNAI lock
     function AddNFTtoLock(uint256 UserLockID, uint256[] calldata NFTs) public returns(bool success){
-        require((UserLocks[msg.sender][UserLockID].BNFTs_Boosting.length + NFTs.length) <= 10, 'Cannot stake more than 10 NFTs per lock')
+        require((UserLocks[msg.sender][UserLockID].BNFTs_Boosting.length + NFTs.length) <= 10, 'Cannot boost with more than 10 NFTs per lock')
         require(TransferInNFTs(NFTs, msg.sender), 'Unable to transfer NFTs to contract');
 
     }
