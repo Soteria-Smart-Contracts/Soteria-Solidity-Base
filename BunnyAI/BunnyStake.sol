4f62aa4e531bea31964112d417931efe961d94b1
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 02:04:13 +0000

    Feb 28, 2023, 9:04 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -46,7 +46,7 @@ contract BunnyDualStake{
         require(BUNAI.transferFrom(msg.sender, address(this), BUNAI_Amount), 'Unable to transfer BUNAI to contract');
 
         uint256 EndTime = (block.timestamp + LockLenghts[Length]);
-        uint256 Payout 
+        uint256 Payout = 
         UserLocks[msg.sender][LatestUserLock++] = Lock()
 
         return(success);
