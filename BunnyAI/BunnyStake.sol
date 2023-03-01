2b11983a3b021e44a256955a6c19b02244c76946
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 19:18:54 +0000

    Mar 1, 2023, 2:18 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -124,7 +124,7 @@ contract BunnyDualStake{
     //Owner Only Functions
 
     //TODO: ChangePayoutMultipliers (Make sure to set minimum so it doesent fucking explode)
-    function SetNewPayoutMultiplier()
+    function SetNewPayoutMultiplier(LockOptions T)
     //TODO: ChangeNFTBoostMultiplier
     //TODO: SetNewOperator
 
