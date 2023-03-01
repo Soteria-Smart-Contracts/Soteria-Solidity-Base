25b5c64c11201add4776eaccf31119c7bd2d20cb
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 19:07:32 +0000

    Mar 1, 2023, 2:07 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -106,9 +106,9 @@ contract BunnyDualStake{
         require(UserLocks[msg.sender][UserLockID].LockEnd <= block.timestamp, 'This lock is still active and it is too early to claim it');
         require(UserLocks[msg.sender][UserLockID].Claimed == false);
 
-
+        uint256 Payout = 
         UserLocks[msg.sender][UserLockID].Claimed = true;
-        
+
 
     }
 
