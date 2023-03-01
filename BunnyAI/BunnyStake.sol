95a5c5576ff6a6caf3fd624022841948696cd600
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 19:17:05 +0000

    Mar 1, 2023, 2:17 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -115,7 +115,8 @@ contract BunnyDualStake{
         TransferOutNFTs(NFTsToTransfer, msg.sender);
         ERC20(BUNAI).transfer(msg.sender, Payout);
 
-        UserLockList[msg.sender][ListIndex[msg.sender][UserLockID]] = UserLockList[msg.sender][(UserLockList[msg.sender].length)]
+        UserLockList[msg.sender][ListIndex[msg.sender][UserLockID]] = UserLockList[msg.sender][(UserLockList[msg.sender].length - 1)];
+        
 
         return(success);
     }
