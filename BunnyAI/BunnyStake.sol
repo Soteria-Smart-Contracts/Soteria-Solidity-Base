e6c01b9e4430901848e9a3243843739627b43851
Darius Tanz <scratchticketseb@gmail.com>
2023-03-01 01:46:34 +0000

    Feb 28, 2023, 8:46 PM

--- BunnyAI/BunnyStake.sol
+++ BunnyAI/BunnyStake.sol
@@ -26,6 +26,7 @@ contract BunnyDualStake{
 
     mapping(address => mapping(uint256 => Lock)) public UserLocks;
     mapping(address => uint256) internal LatestUserLock;
+    mapping()
 
     //Make events, constructor, etc...
 
@@ -35,7 +36,7 @@ contract BunnyDualStake{
         require(BUNAI_Amount >= MinimumStake, 'You must stake atleast the minimum stake Amount');
         require(BUNAI.transferFrom(msg.sender, address(this), BUNAI_Amount), 'Unable to transfer BUNAI to contract');
 
-        
+
         UserLocks[msg.sender][LatestUserLock++] = Lock()
 
         return(success);
