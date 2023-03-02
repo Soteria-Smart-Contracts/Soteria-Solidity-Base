bded2b4abc29a7443e1a1819f5e92a74ab44a969
Darius Tanz <scratchticketseb@gmail.com>
2023-03-02 13:29:12 +0000

    Mar 2, 2023, 8:29 AM

--- BunnyAI/BunnyNFT.sol
+++ BunnyAI/BunnyNFT.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HopperNFT is ERC721Enumerable, Ownable {
+contract BUNAInft is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
