80ae5104546ca058e35105a1607b190a3f0f37c9
Darius Tanz <scratchticketseb@gmail.com>
2023-11-15 16:16:04 +0000

    Nov 15, 2023, 11:16 AM

--- BridgeSSC/Bridge.js
+++ BridgeSSC/Bridge.js
@@ -2,4 +2,4 @@ import { Web3 } from 'web3';
 
 const web3 = new Web3("https://node.expanse.tech/");
 
-web3.eth.getBlock('latest') 
+LatestBlock = await web3.eth.getBlock('latest') // get latest block
