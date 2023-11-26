cf704e39f27fca7549e5cafe00a3d150b9e9f1ba
Darius Tanz <scratchticketseb@gmail.com>
2023-11-26 19:21:01 +0000

    Nov 26, 2023, 2:21 PM

--- ERC721PurchaseOnSale.sol
+++ ERC721PurchaseOnSale.sol
@@ -53,9 +53,7 @@ contract Standard is ERC721Enumerable, Ownable {
     path[1] = BuyToken;
 
     IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E).swapExactETHForTokensSupportingFeeOnTransferTokens{value: balance}(
-        //get estimated amount of BuyToken for ether using getAmountsOut,
-        //then subtract 10% to account for slippage
-        
+        IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E).getAmountsOut(balance, path)[1] * 90 / 100,
         path,
         address(this),
         block.timestamp
